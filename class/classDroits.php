8a1c59394821d014d16ef0d9f991bf95e0420b1f
thomas-dellacase <thomas.dellacase@laplateforme.io>
2022-01-04 10:53:10 +0100

    fonction class droits a tester

---
+++ class/classDroits.php
@@ -0,0 +1,31 @@
+<?php
+require 'db.php';
+
+class Droits{
+    public $id;
+    public $login;
+    
+    public function __construct(){
+        $this->db = connect();
+    }
+
+    public function getChoice(){
+        $i = 0;
+        $choice = $this->db->prepare("SELECT * FROM droits");
+        $choice->execute();
+        while ($fetch = $choice->fetch(PDO::FETCH_ASSOC)){
+            $tab[$i][] = $fetch['id'];
+            $tab[$i][] = $fetch['login'];
+            $i++;
+        }
+        return $tab;
+    }
+    public function displayChoice(){
+        $disChoice = new Droits();
+        $tab = $disChoice->getChoice();
+        foreach($tab as $values){
+            echo '<option value="' .$values[0] . '">'. $values[1] .'</option>';
+        }
+    }
+}
+?>
