351e797791dd929b9052019200d32a3c99e6774d
yacine-boucetta <yacine.boucetta@laplateforme.io>
2022-01-07 11:37:04 +0100

    test

--- class/classArticles.php
+++ class/classArticles.php
@@ -1,35 +1,33 @@
 <?php
 require 'db.php';
-class Articles(){
 
+class Articles{
 
     public $id_categories;
     public $articles;
     public $date;
     public $id_utilisateur;
-
+    
     function __construct() {
         $this->db=connect();
     }
 
-
-
 //------------------------------affiche article-----------------
-    public getArticle(){
-        $new_art=$this->prepare("SELECT article,nom,login,id_categorie,date FROM articles INNER JOIN utilisateurs ON articles.id_utilisateur= utilisateurs.login INNER JOIN categories ON categories.id = articles.id_categorie ")
+    public function getArticle(){
+        $new_art=$this->prepare("SELECT article,nom,login,id_categorie,date FROM articles INNER JOIN utilisateurs ON articles.id_utilisateur= utilisateurs.login INNER JOIN categories ON categories.id = articles.id_categorie ");
         $new_art->execute();
         $affichearticle=$new_art->fetchAll(PDO::FETCH_ASSOC);
     }
 
 //------------------------------creer article-----------------
 
-    public createArticle(){
-        if(isset($_SESSION['user'])){
-            $login=$_SESSION['user']['login'];
-            $take_id=$this->db->prepare("SELECT id FROM `utilisateurs` WHERE `login`= :login ");
-            $take_id->execute(array(':login' => $login));
-            $get_id=$take_id->fetchall(PDO::FETCH_ASSOC);
-            }
+    public function createArticle(){
+        // if(isset($_SESSION['user'])){
+        //     $login=$_SESSION['user']['login'];
+        //     $take_id=$this->db->prepare("SELECT id FROM `utilisateurs` WHERE `login`= :login ");
+        //     $take_id->execute(array(':login' => $login));
+        //     $get_id=$take_id->fetchall(PDO::FETCH_ASSOC);
+        //     }
 
         if(isset($_POST['envoyer'])){
             $peudo=$_SESSION['user']['id'];
@@ -37,7 +35,7 @@ class Articles(){
             $connexion=$this->db->prepare("INSERT INTO `articles`(`article`, `id_utilisateur`,`id_categorie`,`date`) VALUES (:comment,:pseudo,:cat,:date)");
             $connexion->execute(array(
                 ':comment'=>$comment,
-                ':pseudo'=>$get_id[0]['id'],
+                ':pseudo'=>$_SESSION['user']['id'],
                 ':cat'=>$get_id_cat,
                 ':date'=>date('Y-m-d H:i:s')
             ));
@@ -46,33 +44,53 @@ class Articles(){
     }
 //----------------------------menu deroulant pour categorie----------------------------------------------------
 
-    public createCat(){
+    public function createCat(){
+        $error='';
         if(isset($_POST['verifier'])){
-            $error='';
-            $categorie=$_POST['categorie'];
-            $check_categorie=$this->db->prepare("SELECT * From `categorie` WHERE `nom`=:cat");
+            $categorie=$_POST['categorie'];   
+            
+            if(empty($categorie)){
+                    $error='veuillez remplir le champ';
+            }
+            else{
+            $check_categorie=$this->db->prepare("SELECT * From `categories` WHERE `nom`=:cat");
             $check_categorie->execute(array(
                 ':cat'=>$categorie
             ));
-            $verif_cat->rowcount($check_categorie);
+            $verif_cat=$check_categorie->rowcount($check_categorie);
 
             if($verif_cat>0){
-                $error='La catégorie existe déjà'
+                $error="La catégorie existe déjà";
             }
 
             else{
-                $insert_cat=$this->db->("INSERT INTO `categorie`(nom) VALUES (:cat)");
+                $insert_cat=$this->db->prepare("INSERT INTO `categories`(nom) VALUES (:cat)");
                 $insert_cat->execute(array(
                     ':cat'=>$categorie
                 ));
             }
     }
+}
     }
 
-    public selectCat(){
-        $check_categorie=$this->db->prepare("SELECT * From `categorie`");
-        $check_categorie->execute()
-        $fetch_cat=$check_categorie->fetch(PDO::FETCH_ASSOC);
+    public function selectCat(){
+        $i= 0;
+        $check_categorie=$this->db->prepare("SELECT * From `categories`");
+        $check_categorie->execute();
+        while($fetch_cat=$check_categorie->fetch(PDO::FETCH_ASSOC)){
+            $tab[$i][] = $fetch_cat['id'];
+            $tab[$i][] = $fetch_cat['nom'];
+            $i++;
+        }
+        return $tab;
+    }
+
+    public function displayCat(){
+        $choice= new Articles();
+        $tab= $choice->selectCat();
+        foreach($tab as $value){
+        echo '<option values"' .$value[0] . '">' . $value[1] . '</option>';
+    }
     }
 
 
